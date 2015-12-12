86516927a5ddbc2f98c496352fa02556a21be145
marcwright <marc.wright@gmail.com>
2015-12-12 15:39:16 -0500

    board working except for reset button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
 $(function(){
 
-  var moves = ['','','','','','','','',''], counter = 0, playerTurn = 'X';
+  var moves, counter, playerTurn;
+
+  var switchTurn = function(clickedCell){
+    if (playerTurn == 'X'){   
+      playerTurn = 'O';
+      $('#board').css('border', '10px solid blue');
+      $(clickedCell).addClass('red').text('Addie');      
+    } else {
+      playerTurn = 'X';
+      $('#board').css('border', '10px solid red');
+      $(clickedCell).addClass('blue').text('Finnman');
+    };  
+  }
+
+  var playerClick = function(){
+    $(this).unbind("click");
+    moves[parseInt($(this).attr('id'))] = playerTurn;
+    counter++;
+    console.log(counter, moves, playerTurn);
+    winConditions();
+    switchTurn($(this));  
+  }; 
+
+  var resetBoard = function(){
+    moves = ['','','','','','','','',''];
+    playerTurn = 'X';
+    counter = 0;
+    console.log(counter, moves, playerTurn);
+    $('.cell').on('click', playerClick).removeClass('red blue').html('');
+    console.log("reset the board function");
+    // $('.cell').on('click', playerClick);
+    $('#board').show().css('border', '10px solid green');
+    $('#winnerDiv').hide();
+  };
 
   var freezeBoard = function(){
-    $('.cell').unbind('click');
-  };
+    $('#board').fadeOut(2000),
+    $('#winnerDiv').fadeIn(4000)
+      .html('<h1>' + playerTurn + ' is the winner!</h1>')
+      return
+    };
 
   var winConditions = function(){
     if (moves[0] == playerTurn && moves[1] == playerTurn && moves[2] == playerTurn){
@@ -28,50 +64,15 @@ $(function(){
     } else if (moves[2] == playerTurn && moves[5] == playerTurn && moves[8] == playerTurn) {
       console.log(playerTurn + ' wins in column 3');
       freezeBoard();
-    } else if (counter == 9) {
+    } else if (counter == 8) {
       console.log("cats!");
+      freezeBoard();
     } else {
       return;
     }
   };
-
-  var switchTurn = function(clickedCell){
-    if (playerTurn == 'X'){   
-      playerTurn = 'O';
-      return function(){
-        $('#board').css('border', '10px solid red');
-        /*$(clickedCell).addClass('red').text('Addie');*/
-      }      
-    } else {
-      playerTurn = 'X';
-      return function(){
-        $('#board').css('border', '10px solid blue');
-        /*$(clickedCell).addClass('blue').text('Finnman');*/
-      };      
-    };  
-  }
-
-  var playerClick = function(){
-    $(this).unbind("click");
-    counter++;
-    moves[parseInt($(this).attr('id'))] = playerTurn;
-    console.log(counter, moves);
-    winConditions();
-    switchTurn($(this));  
-  }; 
-
-  var resetBoard = function(){
-    moves = ['','','','','','','','',''];
-    playerTurn = 'X';
-    counter = 0;
-    $('.cell').removeClass('red blue').html('');
-    console.log("reset the board function");
-    $('.cell').on('click', playerClick);
-  };
-
-  $('.cell').on('click', playerClick)
-  $('#resetButton').on('click', resetBoard)
-
-
-
+  
+  // $('.cell').on('click', playerClick);
+  $('#resetButton').on('click', resetBoard);
+  $('#winnerDiv').hide();
 });
