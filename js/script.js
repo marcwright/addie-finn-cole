597a73874f193e2099d6114822a4958fa960e503
marcwright <marc.wright@gmail.com>
2015-12-15 06:44:10 -0500

    pushing to github

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
 $(function() {
 
   var moves, counter, playerTurn, playerOne, playerTwo, players = [];
+  
+//Starts the game. It's called at the bottom of the script.
+  var startGame = function() {
+    $('.resetButton').hide();
+    $("#appBody").show();
+    $('#container').hide();
+    $('.choice').hide();
+    $('#clickChoice').typed({
+      strings: ["Welcome to Tic-Tac-Toe ^1000 <br> With Addie, Finn and Cole!"],
+      typeSpeed: 1,
+      showCursor: false,
+      callback: function() {
+        $('.choice').show().addClass('animated bounceInUp');
+        $('#clickChoice').addClass('animated flash');
+        setTimeout(function(){
+          choosePlayerOne();}, 2000);
+      }
+    });    
+  };
+  
+//Lets the player choose his/her avatar.
+  var choosePlayerOne = function() {
+    $('#clickChoice').empty().delay(2000).text("Player #1, click your avatar!");
+    
+    $('.choice').on('click', function(){
+      $(this).off("click").animate({"opacity": "0.5"});
+      $('#this.id').prepend('<div style="border-top:50px">Player #1 is<br>' + this.id + '!</div>');
+      players.push($(this).attr('id'));
+      $('#clickChoice').text("Player #2, click on your avatar!");
+      $(this).prepend('<p>Player #' + players.length + ' is<br>' + this.id + '!</p>');
+      console.log(players);
+
+      if (players.length == 2){
+        $('#clickChoice').text(players[0] + " vs. " + players[1] + " Let's Play!!");
+        setTimeout(function(){
+          $('#clickChoice').addClass('animated bounceOut');
+          resetBoard();
+        }, 2000);
+      }
+    })
+  };
 
   var switchTurn = function(clickedCell) {
     if (playerTurn == players[0]) {
@@ -44,7 +85,7 @@ $(function() {
     $('#winnerDiv').hide();
   };
 
-  var freezeBoard = function() {
+  var winnerIs = function() {
     $('#board').addClass('animated hinge'),
       $('#winnerDiv').fadeIn(4000).addClass(playerTurn).css('background-size', 'contain')
       .html("<h1 class='animated wobble'>" + playerTurn + ' wins!</h1>');
@@ -58,79 +99,42 @@ $(function() {
   var winConditions = function() {
     if (moves[0] == playerTurn && moves[1] == playerTurn && moves[2] == playerTurn) {
       console.log(playerTurn + ' wins on row 1');
-      freezeBoard();
+      winnerIs();
     }
     else if (moves[3] == playerTurn && moves[4] == playerTurn && moves[5] == playerTurn) {
       console.log(playerTurn + ' wins on row 2');
-      freezeBoard();
+      winnerIs();
     }
     else if (moves[6] == playerTurn && moves[7] == playerTurn && moves[8] == playerTurn) {
       console.log(playerTurn + ' wins on row 3');
-      freezeBoard();
+      winnerIs();
     }
     else if (moves[0] == playerTurn && moves[4] == playerTurn && moves[8] == playerTurn || moves[2] == playerTurn && moves[4] == playerTurn && moves[6] == playerTurn) {
       console.log(playerTurn + ' wins on the diagonal');
-      freezeBoard();
+      winnerIs();
     }
     else if (moves[0] == playerTurn && moves[3] == playerTurn && moves[6] == playerTurn) {
       console.log(playerTurn + ' wins in column 1');
-      freezeBoard();
+      winnerIs();
     }
     else if (moves[1] == playerTurn && moves[4] == playerTurn && moves[7] == playerTurn) {
       console.log(playerTurn + ' wins in column 2');
-      freezeBoard();
+      winnerIs();
     }
     else if (moves[2] == playerTurn && moves[5] == playerTurn && moves[8] == playerTurn) {
       console.log(playerTurn + ' wins in column 3');
-      freezeBoard();
+      winnerIs();
     }
     else if (counter == 8) {
       console.log("cats!");
-      freezeBoard();
+      winnerIs();
     }
     else {
       return;
     }
   };
 
-  var choosePlayerOne = function() {
-    $('#clickChoice').empty().delay(2000).text("Player #1, click your avatar!");
-    
-    $('.choice').on('click', function(){
-      $(this).off("click").animate({"opacity": "0.5"});
-      $('#this.id').prepend('<div style="border-top:50px">Player #1 is<br>' + this.id + '!</div>');
-      players.push($(this).attr('id'));
-      $('#clickChoice').text("Player #2, click on your avatar!");
-      $(this).prepend('<p>Player #' + players.length + ' is<br>' + this.id + '!</p>');
-      console.log(players);
 
-      if (players.length == 2){
-        $('#clickChoice').text(players[0] + " vs. " + players[1] + " Let's Play!!");
-        setTimeout(function(){
-          $('#clickChoice').addClass('animated bounceOut');
-          resetBoard();
-        }, 2000);
-      }
-    })
-  };
-
-  var startGame = function() {
-    $('.resetButton').hide();
-    $("#appBody").show();
-    $('#container').hide();
-    $('.choice').hide();
-    $('#clickChoice').typed({
-      strings: ["Welcome to Tic-Tac-Toe ^1000 <br> With Addie, Finn and Cole!"],
-      typeSpeed: 1,
-      showCursor: false,
-      callback: function() {
-        $('.choice').show().addClass('animated bounceInUp');
-        $('#clickChoice').addClass('animated flash');
-        setTimeout(function(){
-          choosePlayerOne();}, 2000);
-      }
-    });    
-  };
 
  
   startGame();
